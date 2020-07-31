7cac13eb49635a3fc0a895029d1bf71af5777abd
Richard Ottaway <richard.ottaway5@gmail.com>
2020-07-31 14:46:48 +0100

    Basic setup with swagger

---
+++ DangerTapeAPI/Controllers/ModesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DangerTapeAPI.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ModesController : ControllerBase
+    {
+        public ModesController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> GetAllModes()
+        {
+            var output = new List<string>
+            {
+                "Ionian",
+                "Dorian"
+            };
+
+            return await Task.FromResult(output);
+        }
+    }
+}
